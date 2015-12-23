148253ae817accd5a03aa98f74c04591bb0ad7e6
saha9307 <saha9307@gmail.com>
2015-12-23 12:52:59 +0200

    Add Toolbar, search button and icon for search button

--- RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,17 +1,38 @@
 package com.qoobico.remindme;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by oleksandr.pachkovsky on 22.12.2015.
  */
 public class MainActivity extends Activity{
 
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
+
+    public void initToolbar(){
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
+
 }
