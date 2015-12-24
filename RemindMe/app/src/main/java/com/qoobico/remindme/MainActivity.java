7ef3e244b7f65ec90a4952500b5e4b142b610cd9
saha9307 <saha9307@gmail.com>
2015-12-24 14:26:01 +0200

    Small correct

--- RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.qoobico.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -10,15 +11,19 @@ import android.view.MenuItem;
  */
 public class MainActivity extends AppCompatActivity{
 
+    private static final int MAIN_LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(MAIN_LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     public void initToolbar(){
@@ -32,6 +37,10 @@ public class MainActivity extends AppCompatActivity{
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 
 
