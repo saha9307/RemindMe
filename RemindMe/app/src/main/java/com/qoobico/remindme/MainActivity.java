1a81389911ba71e840f8557f315f862c4bc7c8f3
saha9307 <saha9307@gmail.com>
2015-12-26 13:53:53 +0200

    Add TableNavigation and Fragment part 2

--- RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,14 +1,18 @@
 package com.qoobico.remindme;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.qoobico.remindme.adapter.TabsPagerFragmentAdapter;
+import com.qoobico.remindme.fragment.Constants;
 
 /**
  * Created by oleksandr.pachkovsky on 22.12.2015.
@@ -47,6 +51,23 @@ public class MainActivity extends AppCompatActivity{
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
     }
 
     private void initTabs() {
@@ -56,6 +77,10 @@ public class MainActivity extends AppCompatActivity{
 
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 
 
---
+++ RemindMe/app/src/main/java/com/qoobico/remindme/fragment/Constants.java
@@ -0,0 +1,10 @@
+package com.qoobico.remindme.fragment;
+
+/**
+ * Created by Олександр on 26.12.2015.
+ */
+public class Constants {
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
